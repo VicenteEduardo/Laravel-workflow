26ca0e3304411d3e0b65e53c61956ecdddfb64e8
root <vicentemanueleduardo@gmail.com>
2024-08-19 09:32:54 +0100

    build laravel

--- .github/actions/conf-laravel/entrypoint.sh
+++ .github/actions/conf-laravel/entrypoint.sh
@@ -1,9 +1,13 @@
-
 #!/bin/sh
 set -e
 
+# Copia o arquivo .env.example para .env, se necessário
 cp .env.example .env
+
+# Executa comandos Laravel
 php artisan cache:clear
-php artisan conf:cache
+php artisan config:cache
 php artisan key:generate
 
+# Executa o composer install para garantir que todas as dependências estejam instaladas
+composer install
